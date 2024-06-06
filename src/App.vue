9190d490867d3bd21a6fc3ca2447dcc84e02fce8
Matthew McClain <mthommcclain@gmail.com>
2024-06-06 10:36:27 -0700

    initial setup demo marked with DOMPurify

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+        
+        <div class="left-col">
+            <h3>Question Markdown</h3>
+            <textarea v-model="userInput" />
+        </div>
+
+        <div class="right-col">
+            <h3>Raw</h3>
+            <div v-html="rawMarkdown"></div>
+            <hr>
+            <h3>Sanitized with DOMPurify</h3>
+            <div v-html="sanitizedMarkdown"></div>
+        </div>
+
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, computed } from 'vue'
+import { marked } from 'marked'
+import DOMPurify from 'DOMPurify'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const userInput = ref(`#### Sanitize Example \n\n Below is a simple, malicious example of html in markdown that we catch with DOM Purify before injecting the html output of 'marked.' \n\n<div>\n  <button onclick="alert('This is an XSS attack!')">\n    Click me\n  </button>\n</div>` )
+const rawMarkdown = computed(() => marked.parse(userInput.value))
+const sanitizedMarkdown = computed(() => DOMPurify.sanitize(rawMarkdown.value))
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.wrapper {
+    display: flex;
+    height: 100vh;
+    width: 100%;
+    justify-content: center;
+}
+.left-col, .right-col {
+    width: 50%;
+    max-width: 400px;
+    height: 100%;
+    padding: 8px;
+}
+textarea {
+    width: 100%;
+    height: 300px;
 }
 </style>
