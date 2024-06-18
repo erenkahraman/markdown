cc2155722aeec98ace8e3889941e7f527b673ae8
Eren Kahraman <110972375+erenkahraman@users.noreply.github.com>
2024-06-18 14:45:14 +0200

    marhjax

---
+++ src/components/MathJax.vue
@@ -0,0 +1,27 @@
+<template>
+    <div ref="content"></div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MathJax',
+    props: {
+      content: {
+        type: String,
+        required: true
+      }
+    },
+    mounted() {
+      this.renderMath();
+    },
+    updated() {
+      this.renderMath();
+    },
+    methods: {
+      renderMath() {
+        this.$refs.content.innerHTML = this.content;
+        window.MathJax.typesetPromise([this.$refs.content]);
+      }
+    }
+  };
+  </script>
